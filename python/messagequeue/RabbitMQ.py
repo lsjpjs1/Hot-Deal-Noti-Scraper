766b0193edb11916ab970ea66c98486a29b730a8
lsjpjs1 <lsjpjs1@naver.com>
2022-07-24 13:46:37 +0900

    fix: 메시지 큐에 보내는 형식 수정

--- python/messagequeue/RabbitMQ.py
+++ python/messagequeue/RabbitMQ.py
@@ -1,3 +1,5 @@
+import json
+
 import pandas as pd
 import pika
 from dotenv import load_dotenv
@@ -16,14 +18,12 @@ class RabbitMQ:
         )
         return
 
-    def publish(self,body):
+    def publish(self, body):
         connection = pika.BlockingConnection(
             pika.ConnectionParameters(self.__url, self.__port, self.__vhost, self.__cred))
-        connection.channel().basic_publish(exchange='amq.direct',routing_key='hi',body=body)
+        connection.channel().basic_publish(exchange='amq.direct', routing_key='hi', body=body)
         connection.close()
 
 
-# frame = pd.DataFrame({"aa": [], "bb": [], "cc": [], "url": []})
-# frame = frame.append({"aa": 13, "bb": 10000, "cc": "하이", "url": "www.naver.com"},ignore_index=True)
 # mq = RabbitMQ()
-# mq.publish(frame.to_json())
+# mq.publish(json.dumps({"aa": "hello", "bb": "hi"}))
