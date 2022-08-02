bc34d64d0a7ebfd6c928ecc879b27a77273b6f6d
lsjpjs1 <lsjpjs1@naver.com>
2022-08-02 20:51:30 +0900

    feat: 크롤링 시작 시 로그 찍히는 기능 추가

--- python/scraper/StartScraping.py
+++ python/scraper/StartScraping.py
@@ -1,9 +1,13 @@
+
 import os
 import sys
+from datetime import datetime
+
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from .Scraper11st import Scraper11st
 from .ScraperGmarket import ScraperGmarket
 
+print(datetime.now(),": 크롤링 시작")
 searchWords = [
     "기가바이트 노트북", "asus 노트북", "lg 노트북", "삼성 노트북", "hp 노트북", "레노버 노트북", "델 노트북"
 ]
