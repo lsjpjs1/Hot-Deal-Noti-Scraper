45237f082b4956a42940b8a1e71562f1a2011df1
lsjpjs1 <lsjpjs1@naver.com>
2022-08-06 03:59:50 +0900

    fix: 크롤링 시작 시 로그 수정

--- python/scraper/StartScraping.py
+++ python/scraper/StartScraping.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from .Scraper11st import Scraper11st
 from .ScraperGmarket import ScraperGmarket
 
-print(datetime.now(),": 크롤링 시작")
+print(datetime.now(),": 크롤링 시작합니다!")
 searchWords = [
     "기가바이트 노트북", "asus 노트북", "lg 노트북", "삼성 노트북", "hp 노트북", "레노버 노트북", "델 노트북"
 ]
