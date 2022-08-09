352b13a27c8a5f0b4f152e80a97b6a841fdcfb9b
lsjpjs1 <lsjpjs1@naver.com>
2022-08-09 21:25:18 +0900

    fix: 지마켓도 키워드 여러 개로 검색하게 수정

--- python/scraper/StartScraping.py
+++ python/scraper/StartScraping.py
@@ -15,16 +15,8 @@ searchWords = [
 scraper11st = Scraper11st()
 scraper11st.startScraping(searchWords)
 
-
-searchWords = [
-    "노트북"
-]
 scraper = ScraperGmarket()
 scraper.startScraping(searchWords)
 
-
-searchWords = [
-    "기가바이트 노트북", "asus 노트북", "lg 노트북", "삼성 노트북", "hp 노트북", "레노버 노트북", "델 노트북"
-]
 scraper_lotte_on = ScraperLotteOn()
 scraper_lotte_on.startScraping(searchWords)
