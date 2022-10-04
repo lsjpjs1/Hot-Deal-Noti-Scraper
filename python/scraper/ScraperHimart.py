a502081b8e68d904634a634c343a8859219dad63
lsjpjs1 <lsjpjs1@naver.com>
2022-10-05 04:46:25 +0900

    feat: 하이마트 크롤러 추가

---
+++ python/scraper/ScraperHimart.py
@@ -0,0 +1,109 @@
+import os
+import sys
+
+
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+import json
+import re
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+from .Scraper import Scraper
+from .WebdriverBuilder import WebdriverBuilder
+
+
+class ScraperHimart(Scraper):
+
+    def initSite(self, driver, searchWord):
+        driver.get("https://www.e-himart.co.kr/")
+        self.wait(driver, (By.XPATH, "//input[@id='topSearchQuery']"))
+        driver.find_element_by_xpath("//input[@id='topSearchQuery']").send_keys(searchWord)
+        self.wait(driver, (By.XPATH, "//a[@id='btnSearch']"))
+        driver.find_element_by_xpath("//a[@id='btnSearch']").click()
+
+    def collectData(self, driver):
+
+        self.waitDuringTime(driver, (
+            By.XPATH,
+            "//div[@class='prdItem ']"),
+                            10)
+        items = driver.find_elements_by_xpath(
+            "//div[@class='prdItem ']")
+
+        comma_won_re = re.compile('([0-9]{1,3}(,[0-9]{3})+)')
+        man_won_re = re.compile('([0-9]+)만')
+        res = {"hotDealMessages":[]}
+        for item in items:
+            try:
+                original_title = item.find_element_by_xpath(".//p[@class='prdName']").text
+                title = original_title.replace(" ", "").replace(".", "")
+                url = item.find_element_by_xpath(".//a[@class='prdLink']").get_attribute("href")
+                original_price = int(
+                    item.find_element_by_xpath(
+                        ".//div[@class='priceInfo']//span[@class='discountPrice']//strong").text.replace(
+                        ",", "")
+                )
+                discount_price = int(
+                    item.find_element_by_xpath(
+                        ".//div[@class='priceInfo priceBenefit']//span[@class='discountPrice']//strong").text.replace(
+                        ",", "")
+                )
+                is_sold_out = len(item.find_elements_by_xpath(".//div[@class='soldout']"))>0
+            except Exception as e:
+                print(e)
+                continue
+            if original_price<300000 or is_sold_out:
+                continue
+            discount_rate=int(100 - 100 * discount_price / original_price)
+
+            if 15 <= discount_rate <= 100:
+                res.get("hotDealMessages").append(
+                            {
+                                "discountRate": discount_rate, "discountPrice": discount_price,
+                                "originalPrice": original_price, "title": original_title,
+                                "url": url, "sourceSite": "하이마트"
+                            }
+                )
+        self.mq.publish(json.dumps(res), 'inputHotDeal')
+        # self.mq.publish(json.dumps(res), 'inputKeywordNotification')
+
+    def getCurrentPage(self, driver):
+        self.wait(driver, (By.XPATH, "//div[@id='pageNavigator']//a[@class='on']"))
+        return int(driver.find_element_by_xpath("//div[@id='pageNavigator']//a[@class='on']").text)
+
+    def goNextPage(self, driver):
+        current_page = self.getCurrentPage(driver)
+        if current_page % 10 == 0:
+            self.wait(driver, (By.XPATH, "//div[@id='pageNavigator']//a[@class='next']"))
+            driver.find_element_by_xpath("//div[@id='pageNavigator']//a[@class='next']").click()
+        else:
+            next_page = str(current_page + 1)
+            self.wait(driver, (By.XPATH, f"//div[@id='pageNavigator']//a[text()='{next_page}']"))
+            driver.find_element_by_xpath(f"//div[@id='pageNavigator']//a[text()='{next_page}']").click()
+
+    def startScraping(self, searchWords):
+        driver = WebdriverBuilder.getDriver()
+        try:
+            for searchWord in searchWords:
+                self.initSite(driver, searchWord)
+                try:
+                    for i in range(35):
+                        self.collectData(driver)
+                        self.goNextPage(driver)
+                        time.sleep(1.5)
+                except Exception as e:
+                    print(e)
+                    continue
+        except Exception as e:
+            print(e)
+        finally:
+            driver.quit()
+
+
+
+
+
--- python/scraper/StartScraping.py
+++ python/scraper/StartScraping.py
@@ -10,6 +10,9 @@ from .Scraper11st import Scraper11st
 from .ScraperGmarket import ScraperGmarket
 from .ScraperLotteOn import ScraperLotteOn
 from .ScraperAuction import ScraperAuction
+from .ScraperHimart import ScraperHimart
+
+
 
 searchWords = [
     "기가바이트 노트북", "asus 노트북", "lg 노트북", "삼성 노트북", "hp 노트북", "레노버 노트북", "델 노트북", "msi 노트북"
@@ -26,3 +29,6 @@ scraper_lotte_on.startScraping(searchWords)
 print(datetime.now(),": 옥션 크롤링 시작합니다!")
 scraper_auction = ScraperAuction()
 scraper_auction.startScraping(searchWords)
+print(datetime.now(),": 하이마트 크롤링 시작합니다!")
+scraperHimart = ScraperHimart()
+scraperHimart.startScraping(searchWords)
