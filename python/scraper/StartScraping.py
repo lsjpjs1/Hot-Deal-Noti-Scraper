0610fbb888b94e495f556bcfcdaef5c74189e4cf
lsjpjs1 <lsjpjs1@naver.com>
2022-10-09 14:20:24 +0900

    feat: 쿠팡 크롤러 추가

--- python/scraper/StartScraping.py
+++ python/scraper/StartScraping.py
@@ -18,6 +18,9 @@ from .ScraperCoupang import ScraperCoupang
 searchWords = [
     "기가바이트 노트북", "asus 노트북", "lg 노트북", "삼성 노트북", "hp 노트북", "레노버 노트북", "델 노트북", "msi 노트북"
 ]
+print(datetime.now(), ": 쿠팡 크롤링 시작합니다!")
+scraperCoupang = ScraperCoupang()
+scraperCoupang.startScraping()
 print(datetime.now(),": 11번가 크롤링 시작합니다!")
 scraper11st = Scraper11st()
 scraper11st.startScraping(searchWords)
@@ -33,6 +36,3 @@ scraper_auction.startScraping(searchWords)
 print(datetime.now(),": 하이마트 크롤링 시작합니다!")
 scraperHimart = ScraperHimart()
 scraperHimart.startScraping(searchWords)
-print(datetime.now(), ": 쿠팡 크롤링 시작합니다!")
-scraperCoupang = ScraperCoupang()
-scraperCoupang.startScraping()
