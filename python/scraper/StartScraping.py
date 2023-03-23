6e914df5815b73c46265129ef192a507321d2640
lsjpjs1 <lsjpjs1@naver.com>
2023-03-23 16:20:39 +0900

    feat: 11번가 가격검증 기능 추가

--- python/scraper/StartScraping.py
+++ python/scraper/StartScraping.py
@@ -20,18 +20,7 @@ from .ScraperWeMakePrice import ScraperWeMakePrice
 #
 # print(datetime.now(),": 위메프 크롤링 시작합니다!")
 # ScraperWeMakePrice(productTypeId=productTypeId).startScraping([
-#     'https://front.wemakeprice.com/category/division/2100132',
-# 'https://front.wemakeprice.com/category/division/2100751',
-# 'https://front.wemakeprice.com/category/division/2100753',
-# 'https://front.wemakeprice.com/category/division/2100754',
-# 'https://front.wemakeprice.com/category/division/2100755',
-# 'https://front.wemakeprice.com/category/division/2100756',
-# 'https://front.wemakeprice.com/category/division/2100757',
-# 'https://front.wemakeprice.com/category/division/2100758',
-# 'https://front.wemakeprice.com/category/division/2100759',
-# 'https://front.wemakeprice.com/category/division/2100760',
-# 'https://front.wemakeprice.com/category/division/2100761',
-# 'https://front.wemakeprice.com/category/division/2100752'
+#     'https://front.wemakeprice.com/category/division/2100135'
 # ])
 #
 # print(datetime.now(),": 11번가 크롤링 시작합니다!")
@@ -54,28 +43,29 @@ from .ScraperWeMakePrice import ScraperWeMakePrice
 
 productTypeId = 1
 searchWords = [
-    "레노버 노트북","레이저 노트북","기가바이트 노트북", "asus 노트북", "lg 노트북", "삼성 노트북", "hp 노트북",  "델 노트북", "msi 노트북"
+    "레노버 노트북","레이저 노트북","기가바이트 노트북", "asus 노트북", "lg 노트북", "노트북 삼성", "hp 노트북",  "델 노트북", "msi 노트북"
 ]
-
-print(datetime.now(),": 위메프 크롤링 시작합니다!")
-ScraperWeMakePrice(productTypeId=productTypeId).startScraping([
-    'https://front.wemakeprice.com/category/division/2100132',
-'https://front.wemakeprice.com/category/division/2100751',
-'https://front.wemakeprice.com/category/division/2100753',
-'https://front.wemakeprice.com/category/division/2100754',
-'https://front.wemakeprice.com/category/division/2100755',
-'https://front.wemakeprice.com/category/division/2100756',
-'https://front.wemakeprice.com/category/division/2100757',
-'https://front.wemakeprice.com/category/division/2100758',
-'https://front.wemakeprice.com/category/division/2100759',
-'https://front.wemakeprice.com/category/division/2100760',
-'https://front.wemakeprice.com/category/division/2100761',
-'https://front.wemakeprice.com/category/division/2100752'
-])
 
 print(datetime.now(),": 11번가 크롤링 시작합니다!")
 scraper11st = Scraper11st(productTypeId=productTypeId)
 scraper11st.startScraping(searchWords)
+
+# print(datetime.now(),": 위메프 크롤링 시작합니다!")
+# ScraperWeMakePrice(productTypeId=productTypeId).startScraping([
+#     'https://front.wemakeprice.com/category/division/2100132',
+# 'https://front.wemakeprice.com/category/division/2100751',
+# 'https://front.wemakeprice.com/category/division/2100753',
+# 'https://front.wemakeprice.com/category/division/2100754',
+# 'https://front.wemakeprice.com/category/division/2100755',
+# 'https://front.wemakeprice.com/category/division/2100756',
+# 'https://front.wemakeprice.com/category/division/2100757',
+# 'https://front.wemakeprice.com/category/division/2100758',
+# 'https://front.wemakeprice.com/category/division/2100759',
+# 'https://front.wemakeprice.com/category/division/2100760',
+# 'https://front.wemakeprice.com/category/division/2100761',
+# 'https://front.wemakeprice.com/category/division/2100752'
+# ])
+
 print(datetime.now(),": 지마켓 크롤링 시작합니다!")
 scraper = ScraperGmarket(productTypeId=productTypeId)
 scraper.startScraping(searchWords)
